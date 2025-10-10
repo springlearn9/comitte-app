aa614330fb3430429bee96411388050e6d817ead
springlearn9 <springlearn9@gmail.com>
2025-10-10 10:00:04 +0530

    vikas: implemented email service to send simple emails

---
+++ src/main/java/com/ls/common/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.ls.common.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendSimpleEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        System.out.println("Email sent successfully to " + toEmail);
+    }
+}
+
